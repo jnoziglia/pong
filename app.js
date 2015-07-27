301855e21af4e1df673357347e90b66dbd41ee5e
mpandolfelli <martinpandolfelli@gmail.com>
2015-07-27 15:14:06 -0300

    agregado socketio, funca con lag

--- app.js
+++ app.js
@@ -18,3 +18,12 @@ var server = require('http').Server(app);
 server.listen(8080, function() {
   console.log("Node server running on http://localhost:8000");
 });
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+ // 
+  socket.on('player1_position_update', function (data) {
+    console.log(data);
+    console.log('llego server');
+    socket.broadcast.emit('player1_position_updated', { player1_position_updated: data.player1_position_update });
+  });
+});
